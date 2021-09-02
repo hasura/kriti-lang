a58a294c83a278fb5846c9f438a8b7268c78c817
jkachmar <git@jkachmar.com>
2021-09-02 16:43:24 -0400

    Updates entry point with new error structure

--- src/Kriti.hs
+++ src/Kriti.hs
@@ -1,14 +1,21 @@
-module Kriti (RenderedError(..), ErrorCode(..), SourcePosition(..), ValueExt(..), runKriti)  where
+module Kriti (
+  Error(..),
+  ErrorCode(..),
+  SourcePosition(..),
+  Span(..),
+  ValueExt(..),
+  runKriti
+)  where
 
 import           Data.Bifunctor    (first)
-import           Kriti.Error
-import           Kriti.Eval
+import           Kriti.Error       (Error(..), ErrorCode(..), SourcePosition(..), Span(..), toError)
+import           Kriti.Eval        (runEval)
 import           Kriti.Lexer       (lexer)
 import           Kriti.Parser      (ValueExt(..), parser)
 import qualified Data.Aeson        as J
 import qualified Data.Text         as T
 
-runKriti :: T.Text -> [(T.Text, J.Value)] -> Either RenderedError J.Value
+runKriti :: T.Text -> [(T.Text, J.Value)] -> Either Error J.Value
 runKriti template source = do
-  template' <- first render $ parser $ lexer template
-  first render $ runEval template' source
+  template' <- first toError . parser $ lexer template
+  first toError $ runEval template' source
