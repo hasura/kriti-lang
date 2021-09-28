7a90fbb7714d429ecf5d13d30065a3bcb37e3f3c
Sameer Kolhar <sameer@hasura.io>
2021-09-29 03:13:42 +0530

    [WIP]: cli for kriti (#18)
    * update Main.hs to be interactive
    
    * update project cabal file
    
    * Serializes kriti output from CLI tool.
    
    Co-authored-by: ssbothwell <ssbothwell@gmail.com>

--- Main.hs
+++ Main.hs
@@ -1,4 +1,59 @@
 module Main where
 
+-- import Control.Exception
+
+import Control.Monad (void)
+import Data.Aeson (decode, encode)
+import Data.Bifoldable (bifoldMap)
+import qualified Data.ByteString.Lazy as BSL
+import Data.Maybe (fromJust)
+import Data.Text
+import Kriti (runKriti)
+import Options.Applicative
+import System.IO (IOMode (ReadMode), openFile)
+
+data KritiOptions = KritiOptions
+  { _koJSONFile :: FilePath,
+    _koTemplateFile :: FilePath,
+    _koBindingSymbol :: Text
+  }
+  deriving (Show, Eq)
+
+-- TODO: we should add support for STDIN too
+kritiOpts :: Parser KritiOptions
+kritiOpts =
+  KritiOptions
+    <$> strOption
+      ( long "json" <> short 'j' <> metavar "JSON_FILE"
+          <> help "The JSON file to read"
+      )
+    <*> strOption
+      ( long "template" <> short 't' <> metavar "TEMPLATE_FILE"
+          <> help "The template file to use"
+      )
+    <*> strOption
+      ( long "bind" <> short 'b' <> metavar "BINDING_SYMBOL"
+          <> showDefault
+          <> value "$"
+          <> help "The symbol that's used to represent a JSON binding within the template"
+      )
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = runKritiInteractive =<< execParser opts
+  where
+    opts =
+      info (kritiOpts <**> helper) $
+        fullDesc
+          <> progDesc "transform JSON using the Kriti language"
+          <> header "kriti - a minimal JSON templating language based on Go's template language."
+
+runKritiInteractive :: KritiOptions -> IO ()
+runKritiInteractive (KritiOptions jsonFile templateFile rootSymbol) = do
+  void $ checkFilePath jsonFile
+  void $ checkFilePath templateFile
+
+  json <- BSL.readFile jsonFile
+  template <- readFile templateFile
+  BSL.putStrLn $ bifoldMap encode encode $ runKriti (pack template) [(rootSymbol, fromJust . decode $ json)]
+  where
+    checkFilePath = flip openFile ReadMode
