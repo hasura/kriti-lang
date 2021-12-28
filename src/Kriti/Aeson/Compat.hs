1744c45623580f022bccc46991923b13236bfdb3
Alex Chen <awchen@fastmail.com>
2021-12-27 18:40:07 -0700

    Reorganize aeson compatibility code to avoid ormolu errors

--- src/Kriti/Aeson/Compat.hs
+++ src/Kriti/Aeson/Compat.hs
@@ -3,33 +3,31 @@
 module Kriti.Aeson.Compat where
 
 #if MIN_VERSION_aeson(2,0,0)
+
 import qualified Data.Aeson.Key as K
 import qualified Data.Aeson.KeyMap as KM
 import Data.Bifunctor (first)
-#else
-import qualified Data.HashMap.Strict as M
-#endif
 import qualified Data.Text as T
 
-type Object v =
-#if MIN_VERSION_aeson(2,0,0)
-  KM.KeyMap v
-#else
-  M.HashMap T.Text v
-#endif
+type Object v = KM.KeyMap v
 
 fromList :: [(T.Text, v)] -> Object v
-fromList =
-#if MIN_VERSION_aeson(2,0,0)
-  KM.fromList . map (first K.fromText)
-#else
-  M.fromList
-#endif
+fromList = KM.fromList . map (first K.fromText)
 
 lookup :: T.Text -> Object v -> Maybe v
-lookup =
-#if MIN_VERSION_aeson(2,0,0)
-  KM.lookup . K.fromText
+lookup = KM.lookup . K.fromText
+
 #else
-  M.lookup
+
+import qualified Data.HashMap.Strict as M
+import qualified Data.Text as T
+
+type Object v = M.HashMap T.Text v
+
+fromList :: [(T.Text, v)] -> Object v
+fromList = M.fromList
+
+lookup :: T.Text -> Object v -> Maybe v
+lookup = M.lookup
+
 #endif
